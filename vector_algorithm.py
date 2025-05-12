90ba042f620e23c8a5f66c197c7b0199994f4137
Arbel <Arbel90@gmail.com>
2025-05-12 13:00:46 +0300

    Deleted old irrelevant files

--- vector_algorithm.py
+++
@@ -1,7 +0,0 @@
-def estimate_gaze(image, faces_matrix) -> list:
-	print("Received image and faces matrix")
-
-	print (f"Image: {image}")
-	print (f"Faces matrix: {faces_matrix}")
-	# This function is a placeholder for the actual gaze estimation algorithm
-	return [1, 1, 1]
